4e43d43c3aa77721fb91b2587e8c5d14c99474a9
sumit kumar dey <sumitkumardey91@yahoo.com>
2019-09-02 18:18:46 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+
+    <p > {{ sum }} </p>
+
+    my result is: <h1> {{ info }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'hi',
+      sum: 10,
+      info: ''
+    }
+  },
+  created() {
+      console.log("created");
+  },
+  mounted() {
+    if (this.sum == 100) {
+      this.info = "success"
+    } else {
+      this.info = "fail"
+    }
+    console.log("mounted")
+  }
+}
+</script>
+
