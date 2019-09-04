9eda64a696a6d69c57a38948e6364d2bb048d557
sumit kumar dey <sumitkumardey91@yahoo.com>
2019-09-04 18:11:09 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div> 
+    <parent-com></parent-com>
+  </div>
+</template>
+
+<script>
+import parent from './components/photoCom/parent.vue'
+
+export default {
+  data() {
+    return {
+     
+    }
+  },
+  components: {
+    'parent-com': parent
+  }
+}
+
+</script>
